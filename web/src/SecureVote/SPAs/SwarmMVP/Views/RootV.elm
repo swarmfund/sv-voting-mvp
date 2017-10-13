a49182cfd811be9c37329854b44a3fbb9d65e8ce
Thomas Sesselmann <sessel52@gmail.com>
2017-10-13 12:03:32 +1100

    fix #9 and fix #10
    - Add castVoteView to the slideHost
    - Add dynamic list of options
    - Add voting view
    - Create BallotOption type alias

--- web/src/SecureVote/SPAs/SwarmMVP/Views/RootV.elm
+++ web/src/SecureVote/SPAs/SwarmMVP/Views/RootV.elm
@@ -16,50 +16,16 @@ import SecureVote.SPAs.SwarmMVP.Msg exposing (Msg(..))
 import Maybe.Extra exposing ((?))
 import SecureVote.SPAs.SwarmMVP.Routes exposing (Route(..))
 import SecureVote.SPAs.SwarmMVP.Views.SwmAddressV exposing (swmAddressV)
+import SecureVote.SPAs.SwarmMVP.Views.SwmCastVoteV exposing (castVoteView)
+import SecureVote.SPAs.SwarmMVP.Views.SwmVotingV exposing (votingView)
 
 
 rootView : Model -> Html Msg
 rootView model =
     slideHost model
         [ ( SwmAddressR, swmAddressV model )
-        , ( SwmVoteR, h1 [] [ text "Slide #2!" ] )
-        ]
-        |> Material.Scheme.top
-
-
-secView : Model -> Html Msg
-secView model =
-    div
-        []
-        [ swmAddressV model
-
-        {- We construct the instances of the Button component that we need, one
-           for the increase button, one for the reset button. First, the increase
-           button. The first three arguments are:
-             - A Msg constructor (`Mdl`), lifting Mdl messages to the Msg type.
-             - An instance id (the `[0]`). Every component that uses the same model
-               collection (model.mdl in this file) must have a distinct instance id.
-             - A reference to the elm-mdl model collection (`model.mdl`).
-           Notice that we do not have to add fields for the increase and reset buttons
-           separately to our model; and we did not have to add to our update messages
-           to handle their internal events.
-           Mdl components are configured with `Options`, similar to `Html.Attributes`.
-           The `Options.onClick Increase` option instructs the button to send the `Increase`
-           message when clicked. The `css ...` option adds CSS styling to the button.
-           See `Material.Options` for details on options.
-        -}
-        --        , Button.render Mdl
-        --            [ 0 ]
-        --            model.mdl
-        --            [ Options.onClick Increase
-        --            , css "margin" "0 24px"
-        --            ]
-        --            [ text "Increase" ]
-        --        , Button.render Mdl
-        --            [ 1 ]
-        --            model.mdl
-        --            [ Options.onClick Reset ]
-        --            [ text "Reset" ]
+        , ( SwmVoteR, castVoteView model )
+        , ( SwmSubmitR, votingView model )
         ]
         |> Material.Scheme.top
 
--- web/src/SecureVote/SPAs/SwarmMVP/Views/SwmCastVoteV.elm
+++ web/src/SecureVote/SPAs/SwarmMVP/Views/SwmCastVoteV.elm
@@ -1,10 +1,56 @@
 module SecureVote.SPAs.SwarmMVP.Views.SwmCastVoteV exposing (..)
 
-import Html exposing (Html, div)
+import Html exposing (Html, div, span, text, p)
+import Html.Attributes exposing (class)
+import Material.Card as Card
+import Material.Color as Color
+import Material.List as Lists
+import Material.Options as Options exposing (cs)
+import Material.Typography exposing (display2, headline)
+import SecureVote.Components.UI.Btn exposing (BtnProps(..), btn)
+import SecureVote.Components.UI.FullPageSlide exposing (fullPageSlide)
 import SecureVote.SPAs.SwarmMVP.Model exposing (Model)
-import SecureVote.SPAs.SwarmMVP.Msg exposing (Msg)
+import SecureVote.SPAs.SwarmMVP.Msg exposing (Msg(ChangePage))
+import SecureVote.SPAs.SwarmMVP.Routes exposing (Route(SwmSubmitR))
+
+
+type alias BallotOption =   
+    { title: String
+    , description: String
+    , params: String
+    }
+
+voteOptions : List BallotOption
+voteOptions =
+    [ BallotOption "Option 1" "Description 1" ""
+    , BallotOption "Option 2" "Description 2" ""
+    , BallotOption "Option 3" "Description 3" ""
+    , BallotOption "Option 4" "Description 4" ""
+    ]
 
 
 castVoteView : Model -> Html Msg
 castVoteView model =
-    div [] []
+    let
+        optionList =
+            List.map optionListItem voteOptions
+
+        optionListItem {title, description, params} =
+            Lists.li []
+                [ Lists.content [] [ text "checkbox" ]
+                , Lists.content2 []
+                    [ text title
+                    , p [] [ text description ]
+                    ]
+                ]
+    in 
+        fullPageSlide 657980946
+            model
+            []
+            [ Card.text [ cs "center tc" ]
+                [ Options.styled span [ display2, Color.text Color.black, cs "db pa2" ] [ text "Swarm Liquidity Vote" ]
+                , Options.styled span [ headline, cs "black dib ba pa3 ma3" ] [ text "Swarm Token Balance: 34,228" ]
+                , Lists.ul [ cs "mw7 center" ] optionList
+                , btn 894823489 model [ PriBtn, Attr (class "mv3"), Click (ChangePage SwmSubmitR) ] [ text "Continue" ]
+                ]
+            ]
