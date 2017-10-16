2c64a294947ffb4afcfa5caeacd70f34a33443bf
Thomas Sesselmann <sessel52@gmail.com>
2017-10-16 17:59:26 +1100

    Add Sliders and Show Details buttons

--- web/src/SecureVote/SPAs/SwarmMVP/Views/SwmCastVoteV.elm
+++ web/src/SecureVote/SPAs/SwarmMVP/Views/SwmCastVoteV.elm
@@ -4,13 +4,15 @@ import Html exposing (Html, div, p, span, text)
 import Html.Attributes exposing (class)
 import Material.Card as Card
 import Material.Color as Color
+import Material.Elevation as Elevation
 import Material.List as Lists
 import Material.Options as Options exposing (cs, css)
+import Material.Slider as Slider
 import Material.Typography exposing (display2, headline)
 import SecureVote.Components.UI.Btn exposing (BtnProps(..), btn)
 import SecureVote.Components.UI.FullPageSlide exposing (fullPageSlide)
 import SecureVote.SPAs.SwarmMVP.Model exposing (Model)
-import SecureVote.SPAs.SwarmMVP.Msg exposing (Msg(ChangePage))
+import SecureVote.SPAs.SwarmMVP.Msg exposing (Msg(ChangePage, SetDialog))
 import SecureVote.SPAs.SwarmMVP.Routes exposing (Route(SwmSubmitR))
 
 
@@ -18,15 +20,21 @@ type alias BallotOption =
     { title : String
     , description : String
     , params : String
+    , btnId : Int
     }
+
+
+dialogView : String -> Html Msg
+dialogView content =
+    div [] [ text content ]
 
 
 voteOptions : List BallotOption
 voteOptions =
-    [ BallotOption "Option 1" "Description 1" ""
-    , BallotOption "Option 2" "Description 2" ""
-    , BallotOption "Option 3" "Description 3" ""
-    , BallotOption "Option 4" "Description 4" ""
+    [ BallotOption "Option 1" "Really long Description Really long Description Really long Description Really long Description Really long Description Really long Description Really long Description Really long Description Really long Description Really long DescriptionReally long Description Really long Description Really long Description Really long Description Really long Description Really long Description Really long Description Really long Description Really long Description Really long DescriptionReally long Description Really long Description Really long Description Really long Description Really long Description Really long Description Really long Description Really long Description Really long Description Really long Description " "" 865456735673
+    , BallotOption "Option 2" "Description 2" "" 7456345623
+    , BallotOption "Option 3" "Description 3" "" 7246474573
+    , BallotOption "Option 4" "Description 4" "" 4573465672
     ]
 
 
@@ -36,30 +44,20 @@ castVoteView model =
         optionList =
             List.map optionListItem voteOptions
 
-        optionListItem { title, description, params } =
+        optionListItem { title, description, params, btnId } =
             Lists.li []
-                [ Card.view
-                    [ css "width" "256px"
+                [ Lists.content []
+                    [ Slider.view
+                        [ Slider.value 0
+                        , Slider.min -3
+                        , Slider.max 3
+
+                        --, Slider.onChange SliderMsg
+                        ]
+                    , div [ class "dib" ] [ text title ]
                     ]
-                    [ Card.title
-                        [ css "background" "url('assets/pomegranate.jpg') center / cover"
-                        , css "height" "256px"
-                        , css "padding" "0" -- Clear default padding to encompass scrim
-                        ]
-                        [ Card.head
-                            [ Options.scrim 0.75
-                            , css "padding" "16px" -- Restore default padding inside scrim
-                            , css "width" "100%"
-                            ]
-                            [ text "Grenadine" ]
-                        ]
-                    , Card.text []
-                        [ Lists.content [] [ text "checkbox" ]
-                        , Lists.content2 []
-                            [ text title
-                            , p [] [ text description ]
-                            ]
-                        ]
+                , Lists.content2 []
+                    [ btn btnId model [ SecBtn, Click (SetDialog (dialogView description)), OpenDialog ] [ text "Show Details" ]
                     ]
                 ]
     in
