7f7986cf69dd1c5fd8081ad608453221b397a61e
Max Kaye <m@xk.io>
2017-10-23 20:35:27 +1100

    Add a bit to loadContractDetails to detect directories and things

--- bin/solidity/loadContractDetails.js
+++ bin/solidity/loadContractDetails.js
@@ -1,15 +1,28 @@
 const fs = require('fs');
+const colors = require('colors');
+
 
 const loadDetails = () => {
     try {
-        const abi = JSON.parse(fs.readFileSync('./_solDist/SwarmVotingMVP.abi').toString());
-        const bin = fs.readFileSync('./_solDist/SwarmVotingMVP.bin').toString();
+        const solDist = "_solDist"
+        var prefix = ""
+        while (true) {
+            if (fs.existsSync("./" + prefix + solDist)) {
+                console.log("Found _solDist at:", "./" + prefix + solDist);
+                break;
+            }
+            prefix += "../"
+        }
+        var prePath = "./" + prefix + solDist;
+        const abi = JSON.parse(fs.readFileSync(prePath + '/SwarmVotingMVP.abi').toString());
+        const bin = fs.readFileSync(prePath + '/SwarmVotingMVP.bin').toString();
         return [abi, bin];
     } catch (err) {
-        log("Failed to find contract details.\n")
-        log("Recieved error:".red, err);
-        log("Jsonified:".magenta, JSON.stringify(err));
-        log("");
+        console.log("Failed to find contract details.\n")
+        console.log("Current working dir is:", process.cwd())
+        console.log("Recieved error:".red, err);
+        console.log("Jsonified:".magenta, JSON.stringify(err));
+        console.log("");
         process.exit(1);
     }
 }
