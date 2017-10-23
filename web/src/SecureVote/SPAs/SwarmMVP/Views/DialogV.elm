0093beb458243c52e64e46c6c92fa5130a421474
Thomas Sesselmann <sessel52@gmail.com>
2017-10-23 16:10:54 +1100

    Add geth dialog boilerplate to resolve #102

--- web/src/SecureVote/SPAs/SwarmMVP/Views/DialogV.elm
+++ web/src/SecureVote/SPAs/SwarmMVP/Views/DialogV.elm
@@ -1,16 +1,17 @@
 module SecureVote.SPAs.SwarmMVP.Views.DialogV exposing (..)
 
-import Html exposing (Html, div, li, span, text, ul)
+import Html exposing (Html, b, div, li, p, pre, span, text, ul)
 import Html.Attributes exposing (class)
 import Material.Options as Options exposing (cs, css)
 import Material.Textfield as Textf
-import Material.Typography exposing (menu)
+import Material.Typography exposing (headline, menu)
 import Maybe.Extra exposing ((?))
 import SecureVote.Components.UI.Btn exposing (BtnProps(..), btn)
 import SecureVote.SPAs.SwarmMVP.DialogTypes exposing (DialogHtml, dialogHtmlRender)
 import SecureVote.SPAs.SwarmMVP.Helpers exposing (getEthNodeTemp, setEthNodeTemp)
 import SecureVote.SPAs.SwarmMVP.Model exposing (Model, initModel)
 import SecureVote.SPAs.SwarmMVP.Msg exposing (Msg(..), ToWeb3Msg(SetProvider))
+import SecureVote.SPAs.SwarmMVP.Views.SwmVotingV exposing (candTxText)
 
 
 settingsDialogV : Model -> Html Msg
@@ -47,9 +48,21 @@ infoDialogV =
     div [] [ text "This is info Dialog Box" ]
 
 
-gethDialogV : Html Msg
-gethDialogV =
-    div [] [ text "This is Geth Dialog Box" ]
+gethDialogV : Model -> Html Msg
+gethDialogV model =
+    div []
+        [ Options.styled div [ headline, cs "black" ] [ text "Ballot Transaction:" ]
+        , div [ class "mw7 ph3 overflow-visible center" ] [ pre [ class "tl" ] [ text <| candTxText model.candidateTx ] ]
+        , Options.styled div [ headline, cs "black" ] [ text "How to send via GETH CLI" ]
+        , p [] [ text loremIpsum ]
+        , b [] [ text "Example Code Snippet:" ]
+
+        -- Might want to use elm-markdown package for code snippets.
+        , div [ class "ba pa3 overflow-scroll nowrap" ]
+            [ p [] [ text "geth --datadir ~/.ethereum_private init ~/dev/genesis.json" ]
+            , p [] [ text "geth --fast --cache 512 --ipcpath ~/Library/Ethereum/geth.ipc --networkid 1234 --datadir ~/.ethereum_private  console" ]
+            ]
+        ]
 
 
 verifyDialogV : Html Msg
@@ -72,3 +85,8 @@ debugDialogV model =
         [ liE "Add whatever you want here for debug"
         , liE <| toString model.ballotBits
         ]
+
+
+loremIpsum : String
+loremIpsum =
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur sagittis semper mi, sit amet laoreet mauris lobortis sed. Sed facilisis justo non sagittis sodales. Proin ornare interdum euismod. Cras ultricies ante vitae convallis viverra. Donec dapibus odio ac metus consequat consectetur vel quis massa. Nunc mattis feugiat erat at porta. Praesent varius felis non ullamcorper condimentum. Ut vitae posuere massa. Aenean vitae euismod mauris. Nunc turpis augue, porttitor at massa eget, gravida vehicula nisl."
