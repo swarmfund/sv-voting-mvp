b172b519c8a5f2d921b5bb6d90f188f34da4a5b2
Thomas Sesselmann <sessel52@gmail.com>
2017-10-24 14:25:07 +1100

    Add MEW link generation to resolve #101

--- web/src/SecureVote/SPAs/SwarmMVP/Views/SwmVotingV.elm
+++ web/src/SecureVote/SPAs/SwarmMVP/Views/SwmVotingV.elm
@@ -7,6 +7,7 @@ import Material.Card as Card
 import Material.Color as Color
 import Material.Options as Options exposing (cs)
 import Material.Typography exposing (display2, headline)
+import Maybe.Extra exposing ((?))
 import SecureVote.Components.UI.Btn exposing (BtnProps(..), btn)
 import SecureVote.Components.UI.FullPageSlide exposing (fullPageSlide)
 import SecureVote.Eth.Encoders exposing (minEthTxEncoder)
@@ -33,7 +34,7 @@ votingView model =
             , Options.styled div [ headline, cs "black" ] [ text "Ballot Transaction:" ]
             , div [ class "mw7 ph3 overflow-visible center" ] [ pre [ class "tl" ] [ text <| candTxText model.candidateTx ] ]
             , div [ class "mv4" ]
-                [ btn 758678435 model [ SecBtn, Attr (class "ph3") ] [ text "Cast using MEW" ]
+                [ btn 758678435 model [ SecBtn, Attr (class "ph3"), Link <| generateMewUri model ] [ text "Cast using MEW" ]
                 , btn 785784536 model [ SecBtn, Attr (class "ph3"), OpenDialog, Click (SetDialog "Cast using Geth" GethDialog) ] [ text "Cast using Geth" ]
                 ]
             , btn 987572349 model [ PriBtn, Attr (class "mv3"), OpenDialog, Click (SetDialog "Verify Your Ballot" VerifyDialog) ] [ text "Verify Ballot" ]
@@ -53,3 +54,38 @@ candTxText candTx =
 
         Just rec ->
             encode 2 <| minEthTxEncoder rec
+
+
+generateMewUri : Model -> String
+generateMewUri model =
+    let
+        candTx =
+            model.candidateTx
+
+        baseUrl =
+            "https://www.myetherwallet.com/?"
+
+        from =
+            "from=" ++ candTx.from ? ""
+
+        to =
+            "&to=" ++ candTx.to ? ""
+
+        value =
+            "&value=" ++ toString candTx.value
+
+        --        sendMode =
+        --            "&sendMode=" ++ candTx.sendMode ? ""
+        --
+        --        tokenSymbol =
+        --            "&tokenSymbol=" ++ candTx.tokenSymbol ? ""
+        --
+        --        gasLimit =
+        --            "&gasLimit=" ++ candTx.gasLimit ? ""
+        data =
+            "&data=" ++ candTx.data ? ""
+
+        tail =
+            "#send-transaction"
+    in
+    baseUrl ++ from ++ to ++ value ++ data ++ tail
