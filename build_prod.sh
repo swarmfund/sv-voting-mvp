8041c0b3982810257974e8a40500cd83dfee0c09
Max Kaye <m@xk.io>
2018-02-13 13:31:08 +1100

    Add Purescript and Elm artifacts to cache for netlify

--- build_prod.sh
+++ build_prod.sh
@@ -6,4 +6,13 @@ if [ $INSTALL_WEBPACK ]; then
   echo "##> Installed webpack."
 fi
 
+# get cached elm stuff
+mkdir -p .cache
+cp -a .cache/elm-stuff elm-stuff || true
+
+# do build
 webpack "$@"
+
+# save in cache
+rm -rf .cache/elm-stuff || true
+cp -a elm-stuff .cache/elm-stuff
