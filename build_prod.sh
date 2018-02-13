e4a5612570f501e50c0031b3f2e7eae53056f608
Max Kaye <m@xk.io>
2018-02-13 14:07:42 +1100

    Add copy and restore of netlify cache

--- build_prod.sh
+++ build_prod.sh
@@ -6,6 +6,10 @@ if [ $INSTALL_WEBPACK ]; then
   echo "##> Installed webpack."
 fi
 
+# prep for netlify cache stuff
+echo "Restoring .cache to netlify: \n`ls $NETLIFY_CACHE_DIR/.cache`\n"
+cp -a $NETLIFY_CACHE_DIR/.cache .cache || true
+
 # get cached elm stuff
 mkdir -p .cache
 cp -a .cache/elm-stuff elm-stuff || true
@@ -29,3 +33,7 @@ cp -a elm-stuff .cache/elm-stuff
 echo "Cache Directory after build: (ls -al .cache)\n"
 ls -al .cache
 echo ""
+
+# save netlify cache
+cp -a .cache $NETLIFY_CACHE_DIR/.cache
+echo "Saved .cache to netlify: \n`ls $NETLIFY_CACHE_DIR/.cache`\n"
