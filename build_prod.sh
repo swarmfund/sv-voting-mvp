7c58375520e9610fa67c6b15d8543905e0be8415
Max Kaye <m@xk.io>
2018-02-13 13:56:13 +1100

    Update build-prod.sh

--- build_prod.sh
+++ build_prod.sh
@@ -10,9 +10,22 @@ fi
 mkdir -p .cache
 cp -a .cache/elm-stuff elm-stuff || true
 
+# print cache directory
+echo "Cache Directory before build:\n"
+ls -al .cache
+echo ""
+
 # do build
+echo "Building now..."
 webpack "$@"
+echo "Build Complete"
 
 # save in cache
+echo "Caching elm stuff"
 rm -rf .cache/elm-stuff || true
 cp -a elm-stuff .cache/elm-stuff
+
+# print cache directory
+echo "Cache Directory after build: (ls -al .cache)\n"
+ls -al .cache
+echo ""
