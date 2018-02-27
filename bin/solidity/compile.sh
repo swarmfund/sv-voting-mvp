8a380eb066f955bf7607e0cab095a2446da18e1c
Max Kaye <m@xk.io>
2018-02-27 23:06:22 +1100

    add abis (might import into UI so need to be present for build)

--- bin/solidity/compile.sh
+++ bin/solidity/compile.sh
@@ -14,15 +14,15 @@ while getopts ":c:" opt; do
     case $opt in
         c)
             CONTRACT_NAME="$OPTARG"
-            ;;
+        ;;
         \?)
             echo "invalid option -$OPTARG"
             exit 1
-            ;;
+        ;;
         :)
             echo "Option -$OPTARG requires an argument"
             exit 1
-            ;;
+        ;;
     esac
 done
 
