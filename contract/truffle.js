c6f62ab16966d22b596002fb2d8e545c27162188
Max Kaye <m@xk.io>
2018-02-27 21:21:07 +1100

    Add architecture doc and slightl aterations to js files

--- contract/truffle.js
+++ contract/truffle.js
@@ -1,16 +1,19 @@
-var TestRPC = require('ethereumjs-testrpc');
+var TestRPC = require("ethereumjs-testrpc");
 
-const {create, env} = require('sanctuary');
-const S = create({checkTypes: true, env});
+const { create, env } = require("sanctuary");
+const S = create({ checkTypes: true, env });
 
-// var provider = TestRPC.provider({port: 34839, accounts: S.map(_ => ({balance: "0xfffffffffffffffff"}), S.range(0,210))})
+var provider = TestRPC.provider({
+  port: 34839,
+  accounts: S.map(_ => ({ balance: "0xfffffffffffffffff" }), S.range(0, 20))
+});
 
 module.exports = {
   networks: {
     development: {
-      // provider: provider,
-      host: "localhost",
-      port: 8545,
+      provider: provider,
+      // host: "localhost",
+      // port: 8545,
       gas: 4500000,
       network_id: "*" // Match any network id
     }
