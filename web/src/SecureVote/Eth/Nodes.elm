44bd96a649d94f69774600b96ec741c0b77dd974
Max Kaye <m@xk.io>
2019-06-18 22:22:59 +1000

    update mainnet node to be full archive

--- web/src/SecureVote/Eth/Nodes.elm
+++ web/src/SecureVote/Eth/Nodes.elm
@@ -9,7 +9,8 @@ type alias NetworkTo a =
 
 ethNodes : NetworkTo String
 ethNodes =
-    { mainnet = "https://mainnet.eth.secure.vote:8545/svLight"
+    -- { mainnet = "https://mainnet.eth.secure.vote:8545/svLight"
+    { mainnet = "https://freely-current-heron.quiknode.io/67b1e42d-8c7f-42ed-8dc9-2038915f92ac/O6HylH2iSOQXPh8x5Psyew==/"
     , kovan = "https://kovan.infura.io/v3/9f8da3240536480e94a63f74769f9da6"
     , ropsten = "https://ropsten.infura.io/someApiKeyShrug"
     }
